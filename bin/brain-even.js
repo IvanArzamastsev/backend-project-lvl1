9fa3d67f1e803276523b8d528c58f274c3225927
IvanArzamastse <Kaktysstyle@icloud.com>
2021-09-17 11:51:00 +0300

    Test problem

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,10 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { getRandomNumber } from '../src/index.js';
+import { getRandomNumber, getUserResponse } from '../src/index.js';
 
-const name = readlineSync.question('May I have your name?: ');
+const gameName = 'brain-even';
+
 console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name?: ');
 console.log(`Hello, ${name}!`);
-let counter = 0;
-while (counter < 3) {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const number = getRandomNumber();
-  console.log(`Question: ${number}`);
-  const response = readlineSync.question('Your answer: ');
-  if (number % 2 === 0) {
-    if (response === 'yes') {
-      console.log('Correct!');
-      counter += 1;
-    } else {
-      console.log(`'${response}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${name}!`);
-      counter += 3;
-    }
-  }
-  if (number % 2 !== 0) {
-    if (response === 'no') {
-      console.log('Correct!');
-      counter += 1;
-    } else {
-      console.log(`'${response}' is wrong answer ;(. Correct answer was 'no'.`);
-      console.log(`Let's try again, ${name}!`);
-      counter += 3;
-    }
-  }
-}
-console.log(`Congratulations, ${name}!`);
+getUserResponse(gameName);
