e5b3b467ce2315ea407f6c698067ede3c7a1f0cf
IvanArzamastse <Kaktysstyle@icloud.com>
2021-09-20 18:15:19 +0300

    Added directory games/

---
+++ games/brain-progression.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { getUserResponse } from '../src/index.js';
+
+const gameName = 'brain-progression';
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name?: ');
+console.log(`Hello, ${name}!`);
+getUserResponse(gameName, name);
